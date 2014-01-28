1cd103e6eeff05e186ad0e66dc43dbca17cfd92c
gauravsing <gauravmandy2@gmail.com>
2014-01-28 15:29:35 +0530

    Phonegap Testing
    Testing

---
+++ Phonegap/views/Form.js
@@ -0,0 +1,38 @@
+﻿KitchenSink.Form = function(params) {
+    var viewModel = {
+        autocomplete: {
+            text: ko.observable(""),
+            cities: KitchenSink.db.cities
+        },
+        "switch": {
+            value: ko.observable(true)
+        },
+        slider: {
+            value: ko.observable(5)
+        },
+        rangeSlider: {
+            minValue: ko.observable(3),
+            maxValue: ko.observable(7)
+        },
+        lookup: {
+            data: ["red", "green", "blue", "yellow"],
+            value: ko.observable(null)
+        },
+        selectbox: {
+            data: ["light", "dark"],
+            value: ko.observable(null)
+        },
+        checkbox: {
+            checked: ko.observable(false)
+        },
+        radioGroup: {
+            items: [
+                { text: "Tea" },
+                { text: "Coffee" },
+                { text: "Juice" }
+            ]
+        }
+    };
+
+    return viewModel;
+};
